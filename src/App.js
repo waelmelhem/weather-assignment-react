b1fc25abea67c14f5afe32c2e2c01f7980347dd7
Isra Abosada <israabosada@Isras-MacBook-Pro.local>
2022-11-25 09:01:26 +0200

    adding resources

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+
+      <header className="app__header">
       </header>
+
+      <main className="app__main">
+      </main>
+
     </div>
   );
 }
+
 
 export default App;
